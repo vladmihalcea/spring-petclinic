1541a455c73fba50a01c18d558d6bbb35abe9b02
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2026-05-10 19:31:51 +0300

    Switch the identifier generator from IDENTITY to SEQUENCE

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -17,10 +17,7 @@ package org.springframework.samples.petclinic.model;
 
 import java.io.Serializable;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 
 /**
  * Simple JavaBean domain object with an id property. Used as a base class for objects
@@ -33,7 +30,7 @@ import jakarta.persistence.MappedSuperclass;
 public class BaseEntity implements Serializable {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Integer id;
 
 	public Integer getId() {
